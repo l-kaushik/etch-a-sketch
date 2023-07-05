28c3827396d5adffaac7699dfc281a5e7b45a4e7
l-kaushik <contact.lokeshkaushik@gmail.com>
2023-07-05 18:23:42 +0530

    added smooth flow of pen on the sketch area

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const container = document.querySelector(".container");
+const style = document.createElement("style");
 
-let grid_resolution = 16;
+let grid_resolution = 20;
 
-for(let i = 0; i<grid_resolution; i++){
+style.textContent = `
+    .grid{
+        height: ${450/grid_resolution}px;
+        width: ${450/grid_resolution}px;
+    }
+`;
+
+document.head.appendChild(style);
+
+for(let i = 0; i<grid_resolution*grid_resolution; i++){
     let temp_grid = document.createElement("div"); 
     container.appendChild(temp_grid);
     temp_grid.classList.add(`grid`);
     temp_grid.classList.add(`grid-${i+1}`);
 }
+
+const gridElements = document.querySelectorAll(".grid");
+
+gridElements.forEach((gridElement,index)=>{
+    gridElement.addEventListener("mousemove",(e)=>{
+        if(e.buttons == 1){
+            gridElement.style.backgroundColor = "blue";
+        }
+    });
+});
