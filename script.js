b73347d3e0997531e79265575c85066f77ac447d
l-kaushik <contact.lokeshkaushik@gmail.com>
2023-07-05 19:39:32 +0530

    added color picker

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector(".container");
 const style = document.createElement("style");
+const colorPicker = document.getElementById("colorPicker");
 
+let color = "#000000";
 let grid_resolution = 20;
 
 style.textContent = `
@@ -24,7 +26,12 @@ const gridElements = document.querySelectorAll(".grid");
 gridElements.forEach((gridElement,index)=>{
     gridElement.addEventListener("mousemove",(e)=>{
         if(e.buttons == 1){
-            gridElement.style.backgroundColor = "blue";
+            gridElement.style.backgroundColor = color;
         }
     });
 });
+
+
+colorPicker.addEventListener("change", (e)=>{
+    color = colorPicker.value;
+})
