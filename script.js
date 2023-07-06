f9fe40b4a8d63db045d5b64901ddd1176fe12b33
l-kaushik <contact.lokeshkaushik@gmail.com>
2023-07-06 17:57:38 +0530

    created random and clear button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.querySelector(".container");
 const style = document.createElement("style");
 const colorPicker = document.getElementById("colorPicker");
 const eraser = document.querySelector(".eraser");
+const randomColor = document.querySelector(".random");
+const clearButton = document.querySelector(".clear");
 
 let color = "#000000";
 let grid_resolution = 20;
@@ -33,13 +35,46 @@ gridElements.forEach((gridElement,index)=>{
 });
 
 
+function randColor(){
+   return Math.floor(Math.random() * 256);
+}
+
+
 colorPicker.addEventListener("change", (e)=>{
     color = colorPicker.value;
-})
+    clearButton.removeAttribute("style");
+    randomColor.removeAttribute("style");
+    eraser.removeAttribute("style");
+});
 
-eraser.addEventListener("click", (e)=>{
+eraser.addEventListener("click", ()=>{
+    clearButton.removeAttribute("style");
+    randomColor.removeAttribute("style");
     eraser.style.backgroundColor = "rgb(79, 20, 189)";
     eraser.style.color = "rgb(221, 220, 220)";
     eraser.style.border = "1px solid rgb(221, 220, 220)";
     color = "white";
-})
+});
+
+randomColor.addEventListener("click", ()=>{
+    eraser.removeAttribute("style");
+    clearButton.removeAttribute("style");
+    randomColor.style.color = "rgb(221, 220, 220)";
+    randomColor.style.border = "1px solid rgb(221, 220, 220)";
+    color = `rgb(${randColor()},${randColor()}, ${randColor()})`;
+    randomColor.style.backgroundColor = color;
+});
+
+
+clearButton.addEventListener("click",()=>{
+    eraser.removeAttribute("style");
+    randomColor.removeAttribute("style");
+    clearButton.style.backgroundColor = "rgb(79, 20, 189)";
+    clearButton.style.color = "rgb(221, 220, 220)";
+    clearButton.style.border = "1px solid rgb(221, 220, 220)";
+
+    gridElements.forEach((gridElement,index)=>{
+        gridElement.style.backgroundColor = "white";
+    });
+
+});
